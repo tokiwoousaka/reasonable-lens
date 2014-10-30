2b8f266b453049c8d37ad4756e5649232eb1f2b8
Tokiwo Ousaka <its.out.of.tune.this.my.music@gmail.com>
2014-10-31 00:16:16 +0900

    Build Settings

--- src/Control/Lens.hs
+++ src/Control/Lens.hs
@@ -1,8 +1,9 @@
 module Control.Lens
   ( module Control.Lens.Lens
   , module Control.Lens.Tuple
+  , module Control.Lens.TH
   ) where
 import Control.Lens.Lens
 import Control.Lens.Tuple
-
+import Control.Lens.TH
 
