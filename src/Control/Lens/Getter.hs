aa428628fe50facfab4aa1705c15b5f49c6ff51a
Tokiwo Ousaka <its.out.of.tune.this.my.music@gmail.com>
2015-01-05 01:16:39 +0900

    Add Getter  and add  function

--- src/Control/Lens/Getter.hs
+++ src/Control/Lens/Getter.hs
@@ -1,21 +1,27 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, RankNTypes #-}
 module Control.Lens.Getter where
 import Control.Monad.State.Class
+import Unsafe.Coerce
 infixl 8 ^.
 
 type Getting r s a = (a -> Accessor r a) -> s -> Accessor r s
+type Getter s a = forall r. Getting r s a
+
 
 foldMapOf :: Getting r s a -> (a -> r) -> s -> r
 foldMapOf l r = runAccessor . l (Accessor . r)
 
-foldOf :: Getting a s a -> s -> a
+foldOf :: Getter s a -> s -> a
 foldOf l v = foldMapOf l id v
+
+to :: (s -> a) -> Getting r s a
+to f g = unsafeCoerce . g . f
 
 (^.) = flip foldOf
 
 ----
 
-use :: MonadState s m => Getting a s a -> m a
+use :: MonadState s m => Getter s a -> m a
 use g = do
   state <- get
   return $ state^.g
