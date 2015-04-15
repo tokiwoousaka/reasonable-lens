088bf522fe1321cbd4b9202927d736bab93a609e
Tokiwo Ousaka <its.out.of.tune.this.my.music@gmail.com>
2015-04-15 19:40:38 +0900

    version up

--- src/Control/Lens/Tuple.hs
+++ src/Control/Lens/Tuple.hs
@@ -15,17 +15,12 @@ class Field3 s t a b | s -> a, t -> b, s b -> t, t a -> s where
 class Field4 s t a b | s -> a, t -> b, s b -> t, t a -> s where
   _4 :: Lens s t a b
 
--- 以下、TemplateHaskell採用を検討中...
-
-----
 
 instance Field1 (a, v) (b, v) a b where
   _1 f (a, b) = fmap (\a' -> (a', b)) (f a)
 
 instance Field2 (v, a) (v, b) a b where
   _2 f (a, b) = fmap (\b' -> (a, b')) (f b)
-
-----
 
 instance Field1 (a, v, w) (b, v, w) a b where
   _1 f (a, b, c) = fmap (\a' -> (a', b, c)) (f a)
@@ -35,8 +30,6 @@ instance Field2 (v, a, w) (v, b, w) a b where
 
 instance Field3 (v, w, a) (v, w, b) a b where
   _3 f (a, b, c) = fmap (\c' -> (a, b, c')) (f c)
-
-----
 
 instance Field1 (a, v, w, x) (b, v, w, x) a b where
   _1 f (a, b, c, d) = fmap (\a' -> (a', b, c, d)) (f a)
@@ -49,3 +42,4 @@ instance Field3 (v, w, a, x) (v, w, b, x) a b where
 
 instance Field4 (v, w, x, a) (v, w, x, b) a b where
   _4 f (a, b, c, d) = fmap (\d' -> (a, b, c, d')) (f d)
+
